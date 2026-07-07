30c1a151cd5e446522796616c88660bc31edda90
rudityas-wahyu <rudityaswahyu@gmail.com>
2026-07-07 08:45:39 +0700

    refactor: remove temporary utility and test scripts
    Clean up the repository by deleting various maintenance scripts, database reset utilities, and temporary testing files used during development.

--- add_hist_note.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let c = fs.readFileSync('src/components/modals/RecordPastServiceModal.tsx', 'utf-8');
-
-c = c.replace(
-  "problem_description: problem,",
-  "problem_description: problem + ' (Historical Record)',"
-);
-
-fs.writeFileSync('src/components/modals/RecordPastServiceModal.tsx', c);
